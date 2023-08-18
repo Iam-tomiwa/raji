0021b40814203762b6f7f23746426f5a825cb82c
Raji Mustapha <rajimustapha30@gmail.com>
2023-08-18 22:32:57 +0100

    project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,9 @@
 import Layout from "components/Layout";
 import Projects from "components/Projects";
 import Image from "next/image";
-import {ProjectType} from "pages";
+import { ProjectType } from "pages";
 
-function ProjectsPage({projects}: {projects: ProjectType[]}) {
+function ProjectsPage({ projects }: { projects: ProjectType[] }) {
   return (
     <Layout>
       <Projects projects={projects} />
@@ -21,7 +21,7 @@ export default ProjectsPage;
 
 export const getStaticProps = async () => {
   let res = require("projects.json");
-  let {projects} = res;
+  let { projects } = res;
 
   return {
     props: {
@@ -29,3 +29,11 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+// {
+//   "title": "Fudi",
+//   "stack": ["ReactJs", "SCSS", "Material UI"],
+//   "description": "Fudi is a food/grocery delivery business. This is a website showcasing what Fudi does.",
+//   "link": "https://www.fudi.ng",
+//   "imgUrl": "/images/fudi.jpg"
+// },
